d90b7319cc1587d3ea855fab456c46664ccb6c9b
learnwithjacksun <codewithjacksun@gmail.com>
2025-04-08 07:28:27 +0100

    .

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -4,7 +4,7 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <div className=" min-h-screen relative">
-      <div className="absolute inset-0 -z-10 h-full w-full bg-transparent bg-[radial-gradient(var(--color-line),transparent_1px)] [background-size:16px_16px]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-transparent bg-[radial-gradient(var(--color-muted),transparent_1px)] [background-size:16px_16px]"></div>
 
         <Header />
         <main className="flex-1">{children}</main>
